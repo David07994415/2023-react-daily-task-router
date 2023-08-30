9d9104f273cdefb109a6c81a9a3d69c59649d43f
David07994415 <david.game.power@gmail.com>
2023-08-30 10:21:13 +0800

    dynamic_route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css';
 import React from 'react'
-import { HashRouter, NavLink, Routes, Route } from 'react-router-dom';
+import { HashRouter, NavLink, Routes, Route,Outlet,useParams  } from 'react-router-dom';
 import { useNavigate, } from 'react-router-dom';
 import { useState, useEffect} from "react";
-
 
 const Todo = () => {
   const [islogin, setIsLogin] = useState(false);
@@ -21,6 +20,26 @@ const Register = () => {
 };
 const Home = () => {
   return <p>這是首頁面</p>;
+};
+const Post =() =>{
+  return <div>
+    <p>這是Post父層頁面</p>
+    <Outlet />
+  </div>
+};
+const PostID1 =() =>{
+  let par = useParams();
+  return <div>
+    <p>這是1st_Post子層頁面</p>
+    <p>PostID1:{par.postID1}</p>  
+  </div> 
+};
+const PostID2 =() =>{
+  let par2 = useParams();
+  return <div>
+    <p>這是2nd_Post2子層頁面</p>
+    <p>PostID2:{par2.postID2}</p> 
+  </div> 
 };
 
 function App() {
@@ -40,12 +59,28 @@ function App() {
           <NavLink to="/todo">
             <p>Todo 頁面</p>
           </NavLink>
+          <NavLink to="/post1">
+            <p>post頁面</p>
+          </NavLink>
+          <NavLink to="/post1/childrenpostpage123">
+            <p>post 詳細頁面</p>
+          </NavLink>
+          <NavLink to="/post2/childrenpostpage456">
+            <p>post2 詳細頁面</p>
+          </NavLink>
+
         </div>
         <Routes>
           <Route extra path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login/>} />
           <Route path="/todo" element={<Todo />} />
+          <Route path="/post1" element={<Post />} >
+            <Route path=":postID1" element={<PostID1 />} />
+          </Route>
+          <Route path="/post2" element={<Post />} >
+            <Route path=":postID2" element={<PostID2 />} />
+          </Route>
         </Routes>
         {/* Routes, Route 練習區 */}
         {/* 練習區 */}
